b347de49a6514e6c73f6348d68a5ce270439b2ad
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-04-29 17:43:17 +0200

    Add files via upload

---
+++ js-object-exo1/main.js
@@ -0,0 +1,29 @@
+// ## Exo 1 
+// ### Créez un objet avec vos valeurs
+// ### 'Nom', 'prenom', 'age', 'taille'
+// ### Affichez votre age via un console.log()
+let moi = {
+    nom: "Van San",
+    prenom: 'Alexis',
+    age: 24,
+    taille: 180
+    
+}
+// ## Exo 2 
+// ### Créez un second et un 3eme personnage (le 3eme personnage doit rester vide)
+let toi = {
+    nom: "Simpson",
+    prenom: "Homer",
+    age: 40,
+    taille: 175
+}
+let troisiemeMoi = {}
+// ## Exo 3
+// ### Remplir les propriétées du 3eme personnage
+// ### Son nom doit valoir le nom du personnage1
+// ### Son age doit valoir celui du personnage2
+// ### Et sa taille doit être rempli avec la valeur que vous souhaitez
+troisiemeMoi.name = "Van San",
+troisiemeMoi.prenom = 'Homer',
+troisiemeMoi.age = 56,
+troisiemeMoi.taille = 180
